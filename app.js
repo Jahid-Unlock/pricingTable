e759b8117e82dc542422cf7c5c34a9fa63b6ff1f
jahidNITS <jahid.nits@gmail.com>
2021-03-23 19:36:31 +0600

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+
+
+// --------- CACHING THE DOM --------------
+
+const mySwitch = document.getElementById("priceSwitch");
+const myAnnTables = document.getElementById("myAnnualTables");
+const myMonTables = document.getElementById("myMonthlyTables");
+
+mySwitch.addEventListener('click', function(){
+
+    console.log("Working as intended");
+
+    myAnnTables.classList.toggle("tables-hidden");
+
+    myMonTables.classList.toggle("tables-hidden");
+});
+
